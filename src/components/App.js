bbbc27ec841d91d10bc298a071d00832bd6924c8
Saurabh Sharma <saurabh.19@outlook.com>
2019-07-13 16:16:53 +0530

    init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className={style.header}>
+        Github Starred Repos
+      </header>
+    </div>
+  );
+}
+
+export default App;
